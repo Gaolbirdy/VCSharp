5266a3410607b01a681546f29aaa5ab572b75e46
Gaolbirdy <gaolbird@yeah.net>
2017-10-02 20:34:39 +0800

    commit

--- 第一季 SIKIC/练习题5-5/Program.cs
+++ 第一季 SIKIC/练习题5-5/Program.cs
@@ -17,15 +17,13 @@ namespace 练习题5_5
             string str = ReadLine();
             string[] strArray = str.Split(' ');
             float[] numArray = new float[strArray.Length];
-            int index = 0;
-            foreach (string item in strArray)
+            for (int i = 0; i < strArray.Length; i++)
             {
-                numArray[index] = ToSingle(item);
-                index++;
+                numArray[i] = ToSingle(strArray[i]);
             }
 
             float[] numArray2 = numArray;
-            WriteLine("Array.Sort()排序结果：");
+            WriteLine("Array.Sort()排序结果："); // 快速排序
             Array.Sort(numArray2);
             foreach (float item in numArray)
             {
@@ -34,15 +32,16 @@ namespace 练习题5_5
 
             // 冒泡排序
             for (int i = 0; i < numArray.Length - 1; i++)
+            //for (int i = 1; i <= numArray.Length - 1; i++)
             {
                 // 最大的数放在最后，从数组第一个元素arr[0]遍历至倒数第二个arr[arr.Length-2],拿该元素与它后面的元素比较并交换
-                // j的每一轮遍历后，都能得到最后的arr[arr.Length - 1 - i]元素是最大的，而之前的元素仍是无序的
+                // j的每一轮遍历后，都能得到最后的arr[arr.Length - 1 - i]是剩余的元素中最大的，而之前的元素仍是无序的
                 for (int j = 0; j < numArray.Length - 1 - i; j++)
+                //for (int j = 0; j < numArray.Length - 1; j++)
                 {
                     if (numArray[j] > numArray[j + 1])
                     {
-                        float temp;
-                        temp = numArray[j];
+                        float temp = numArray[j];
                         numArray[j] = numArray[j + 1];
                         numArray[j + 1] = temp;
                     }
