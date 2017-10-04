7ede2ac3ba77856728d6110b7489c553a16d26ac
Gaolbirdy <gaolbird@yeah.net>
2017-10-04 08:15:31 +0800

    commit

--- 第一季 SIKIC/练习题5-8/Program.cs
+++ 第一季 SIKIC/练习题5-8/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using static System.Console;
 using static System.Convert;
+using System.Collections.Generic;
 
 //8，有n(n<=100)个整数，已经按照从小到大顺序排列好，现在另外给一个整数x，请将该数插入到序列中，并使新的序列仍然有序。
 //	输出新的序列
