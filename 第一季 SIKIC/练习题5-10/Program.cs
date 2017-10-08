d289a931e1278d85038dfbd248f235b52374c2e2
Gaolbirdy <gaolbird@yeah.net>
2017-10-08 08:03:06 +0800

    commit

---
+++ 第一季 SIKIC/练习题5-10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//10，输入一个字符串，判断其是否是C#的合法标识符。
+
+namespace 练习题5_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- 第一季 SIKIC/练习题5-9/Program.cs
+++ 第一季 SIKIC/练习题5-9/Program.cs
@@ -12,45 +12,37 @@ namespace 练习题5_9
         // 最少需要准备多少张人民币，即需要优先用大面值的人民币支付
         static void Main(string[] args)
         {
-            int[] salary = {2064, 1231, 24332, 34534, 345, 4243 };
-            
-            int[] faceValue = {100, 50, 10, 5, 2, 1 };
-            int[] countEachValue = new int[faceValue.Length];
+            int[] salary = { 2064, 1231, 24332, 34534, 345, 4243 };
+
+            // 改用二维数组存储面值及相应需要的数量，6行2列
+            int[,] countEachValue = new int[6, 2] { { 100, 0 }, { 50, 0 }, { 10, 0 }, { 5, 0 }, { 2, 0 }, { 1, 0 } };
+            // 用两个数组存储面值和数量
+            //int[] faceValue = {100, 50, 10, 5, 2, 1 };
+            //int[] countEachValue = new int[faceValue.Length];
             int totalCount = 0;
 
             for (int i = 0; i < salary.Length; i++)
             {
+                Write("发工资：" + salary[i] + "， 需要 ");
                 int count = 0;
                 int remainder = salary[i];
-                Write("发工资：" + remainder + "， 需要 ");
-                for (int j = 0; j < faceValue.Length; j++)
+                for (int j = 0; j < countEachValue.GetLength(0); j++)
                 {
-                    count = remainder / faceValue[j];
-                    remainder = remainder % faceValue[j];
-                    Write($"面值 {faceValue[j]} {count}张， ");
-                    countEachValue[j] += count;
+                    count = remainder / countEachValue[j, 0];
+                    remainder = remainder % countEachValue[j, 0];
+                    Write($"面值{countEachValue[j, 0]} {count}张， ");
+                    countEachValue[j, 1] += count;
                     totalCount += count;
                 }
                 WriteLine();
             }
 
-            //totalCount = Sum(countEachValue);
-            Write($"最少需要准备 {totalCount} 张人民币");
-            for (int i = 0; i < faceValue.Length; i++)
+            Write($"\n最少需要准备 {totalCount} 张人民币 ");
+            for (int i = 0; i < countEachValue.GetLength(0); i++)
             {
-                Write($"面值 {faceValue[i]} {countEachValue[i]} 张， ");
+                Write($"面值{countEachValue[i, 0]} {countEachValue[i, 1]} 张， ");
             }
             ReadKey();
         }
-
-        //static int Sum(int[] intArray)
-        //{
-        //    int sum = 0;
-        //    foreach (int item in intArray)
-        //    {
-        //        sum += item;
-        //    }
-        //    return sum;
-        //}
     }
 }
