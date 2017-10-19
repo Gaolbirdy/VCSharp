bb7e7b59a988638a8ccd03ffc43b7cc00ab5f5e3
liukuo <liukuo@gitlab.com>
2017-10-19 08:10:43 +0800

    commit

---
+++ 第一季 SIKIC/练习题5-11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//11，“回文串”是一个正读和反读都一样的字符串，比如“level”或者“noon”等等就是回文串。请写一个程序判断读入的字符串是否是“回文”。
+
+namespace 练习题5_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
