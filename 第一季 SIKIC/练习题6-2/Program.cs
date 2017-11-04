f3d6e2eea491fcd977f012b9327bd2e547725400
Gaolbirdy <gaolbird@yeah.net>
2017-11-04 14:14:51 +0800

    commit

---
+++ 第一季 SIKIC/练习题6-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
+
+//2，f(n)=f(n-1)+f(n-2) f(0)=2 f(1)=3 ,用程序求得f(40)
+
+namespace 练习题6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("输入一个数字：");
+            int num = ToInt32(ReadLine());
+            WriteLine($"F({num})的结果为： " + Fibonacci(num));
+            ReadKey();
+        }
+
+        static int Fibonacci(int n)
+        {
+            int result = 0;
+            if (n < 0)
+                result = 0;
+            else if (n == 0)
+                result = 2;
+            else if (n == 1)
+                result = 3;
+            else
+                result = Fibonacci(n - 1) + Fibonacci(n - 2);
+            return result;
+        }
+    }
+}
---
+++ 第一季 SIKIC/练习题6-3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
+
+//3，将一个正整数分解质因数。例如：输入90,打印出90=2*3*3*5。测试数据有多组
+
+namespace 练习题6_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("输入一个正整数：");
+            int num = ToInt32(ReadLine());
+            int result;
+            for (int i = 0; i < num; i++)
+            {
+                if (IsPrime(i))
+                {
+                    if (num % i == 0)
+                    {
+                        num = num / i;
+                    }
+                }
+                else
+                    break;
+            }
+        }
+
+        static bool IsPrime(int x)
+        {
+            bool isPrime = true;
+            for (int i = 2; i < x; i++)
+            {
+                if ((x % i) != 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            return isPrime;
+        }
+    }
+}
