2f24976c8a18143e728339ea00b9f8c454f1d79e
Gaolbirdy <gaolbird@yeah.net>
2017-11-05 14:18:04 +0800

    commit

--- 第一季 SIKIC/练习题6-3/Program.cs
+++ 第一季 SIKIC/练习题6-3/Program.cs
@@ -16,30 +16,44 @@ namespace 练习题6_3
         {
             WriteLine("输入一个正整数：");
             int num = ToInt32(ReadLine());
-            int result;
-            for (int i = 0; i < num; i++)
+            string result = num + " = ";
+
+            for (int i = 1; i <= num; i++)    // i为寻找的因子
             {
+                if (num == 1 || IsPrime(num))
+                {
+                    result += num;  // 是1，或是质数，则质因子即为该数本身；将质因子拼接进等式字符串
+                    break;  // 即找到了最后一个质因子，结束循环
+                }
                 if (IsPrime(i))
                 {
                     if (num % i == 0)
                     {
-                        num = num / i;
+                        result += i + " * ";  // 将质因子拼接进等式字符串
+                        num = num / i;  // 找到一个质因子后，得到商作为正整数，继续循环找下一个质因子
+                        i = 1 - 1;    // 找到一个质因子后，把i重置为0，在循环后的i++后，i被重置为起始的因子数1；对商从头开始找质因子
+                        continue;
                     }
                 }
-                else
-                    break;
             }
+            WriteLine(result);
+            ReadKey();
         }
 
         static bool IsPrime(int x)
         {
             bool isPrime = true;
-            for (int i = 2; i < x; i++)
+            if (x == 1)
+                isPrime = false;
+            else
             {
-                if ((x % i) != 0)
+                for (int i = 2; i < x; i++)
                 {
-                    isPrime = false;
-                    break;
+                    if ((x % i) == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
                 }
             }
             return isPrime;
---
+++ 第一季 SIKIC/练习题6-4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//4，输入两个正整数m和n，求其最大公约数和最小公倍数。
+
+namespace 练习题6_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
