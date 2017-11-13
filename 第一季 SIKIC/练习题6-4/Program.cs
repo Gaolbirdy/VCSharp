a06a2c1832afcd874f7681faff030781338db42d
liukuo <liukuo@gitlab.com>
2017-11-13 08:31:51 +0800

    commit

--- 第一季 SIKIC/练习题6-4/Program.cs
+++ 第一季 SIKIC/练习题6-4/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
 
 //4，输入两个正整数m和n，求其最大公约数和最小公倍数。
 
@@ -12,6 +14,11 @@ namespace 练习题6_4
     {
         static void Main(string[] args)
         {
+            WriteLine("输入第一个正整数：");
+            int num1 = ToInt32(ReadLine());
+            WriteLine("输入第二个正整数：");
+            int num2 = ToInt32(ReadLine());
+
         }
     }
 }
