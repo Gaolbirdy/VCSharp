c9583faa4b79d2121de541c2786ef9517daab626
Gaolbirdy <gaolbird@yeah.net>
2017-12-07 06:06:05 +0800

    commit

---
+++ RUNOOB/特性/Program.cs
@@ -0,0 +1,33 @@
+﻿#define DEBUG
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+//namespace 特性
+//{
+//    [AttributeUsage(AttributeTargets.Class |
+//        AttributeTargets.Constructor |
+//        AttributeTargets.Field |
+//        AttributeTargets.Method |
+//        AttributeTargets.Property,
+//        AllowMultiple = true)]
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+//        }
+//    }
+//}
+
+public class MyClass
+{
+    [Conditional("DEBUG")]
+    public static void Message(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+}
