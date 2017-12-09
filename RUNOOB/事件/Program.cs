82984b39abfee96a193d8e66c3ecbc35728b2160
liukuo <liukuo@gitlab.com>
2017-12-09 09:35:36 +0800

    commit

---
+++ RUNOOB/事件/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+//namespace SimpleEvent
+//{
+//    /***********发布器类***********/
+//    public class EventTest
+//    {
+//        private int value;
+
+//        public delegate void NumManipulationHandler();
+
+//        public event NumManipulationHandler ChangeNum;
+
+//        protected virtual void OnNumChanged()
+//        {
+//            if (ChangeNum != null)
+//            {
+//                ChangeNum();    /* 事件被触发 */
+//            }
+//            else
+//            {
+//                Console.WriteLine("event not fire");
+//                Console.ReadKey();
+//            }
+//        }
+
+//        //public bool HasEvent()
+//        //{
+//        //    return (ChangeNum != null) ? true : false;
+//        //}
+
+//        public EventTest()
+//        {
+//            int n = 5;
+//            SetValue(n);
+//        }
+
+//        public void SetValue(int n)
+//        {
+//            if (value != n)
+//            {
+//                value = n;
+//                OnNumChanged();
+//            }
+//        }
+//    }
+
+//    /***********订阅器类***********/
+//    public class SubscribEvent
+//    {
+//        public void PrintF()
+//        {
+//            Console.WriteLine("event fire");
+//            Console.ReadKey();
+//        }
+//    }
+
+//    /***********触发***********/
+//    public class MainClass
+//    {
+//        public static void Main()
+//        {
+//            EventTest e = new EventTest();  /* 实例化对象,第一次没有触发事件 */
+//            SubscribEvent v = new SubscribEvent();  /* 实例化对象 */
+//            e.ChangeNum += new EventTest.NumManipulationHandler(v.PrintF);  /* 注册 */
+//            e.SetValue(7);
+//            e.SetValue(11);
+//            e.SetValue(11);
+//        }
+//    }
+//}
+
+namespace BoilerEventAppl
+// 本实例提供一个简单的用于热水锅炉系统故障排除的应用程序。当维修工程师检查锅炉时，锅炉的温度和压力会随着维修工程师的备注自动记录到日志文件中。
+{
+    // boiler 类
+    class Boiler
+    {
+        private int temp;
+        private int pressure;
+
+        public Boiler(int t, int p)
+        {
+            temp = t;
+            pressure = p;
+        }
+
+        public int GetTemp()
+        {
+            return temp;
+        }
+
+        public int GetPressure()
+        {
+            return pressure;
+        }
+    }
+
+    // 事件发布器
+    class DelegateBoilerEvent
+    {
+        public delegate void BoilerLogHandler(string status);
+
+        // 基于上面的委托定义事件
+        public event BoilerLogHandler BoilerEventLog;
+
+        public void LogProcess()
+        {
+            string remarks = "O. K";
+            Boiler b = new Boiler(100, 12);
+            int t = b.GetTemp();
+            int p = b.GetPressure();
+            if (t > 150 || t < 80 || p < 12 || p > 15)
+            {
+                remarks = "Need Maintenance";
+            }
+            OnBoilerEventLog("Logging Info:\n");
+            OnBoilerEventLog("Temparature " + t + "\nPressure: " + p);
+            OnBoilerEventLog("\nMessage: " + remarks);
+        }
+
+        protected void OnBoilerEventLog(string message)
+        {
+            if (BoilerEventLog != null)
+            {
+                BoilerEventLog(message);
+            }
+        }
+    }
+
+    // 该类保留写入日志文件的条款
+    class BoilerInfoLogger
+    {
+        FileStream fs;
+        StreamWriter sw;
+
+        public BoilerInfoLogger(string filename)
+        {
+            fs = new FileStream(filename, FileMode.Append, FileAccess.Write);
+            sw = new StreamWriter(fs);
+        }
+
+        public void Logger(string info)
+        {
+            sw.WriteLine(info);
+        }
+
+        public void Close()
+        {
+            sw.Close();
+            fs.Close();
+        }
+    }
+
+    // 事件订阅器
+    public class RecordBoilerInfo
+    {
+        static void Logger(string info)
+        {
+            Console.WriteLine(info);
+        }//end of Logger
+
+        static void Main()
+        {
+            BoilerInfoLogger fileLog = new BoilerInfoLogger("boiler.txt");
+            DelegateBoilerEvent boilerEvent = new DelegateBoilerEvent();
+            boilerEvent.BoilerEventLog += new DelegateBoilerEvent.BoilerLogHandler(Logger);
+            boilerEvent.BoilerEventLog += new DelegateBoilerEvent.BoilerLogHandler(fileLog.Logger);
+            boilerEvent.LogProcess();
+            Console.ReadLine();
+            fileLog.Close();
+        }//end of main
+
+    }//end of RecordBoilerInfo
+}
--- RUNOOB/委托/Program.cs
+++ RUNOOB/委托/Program.cs
@@ -3,52 +3,98 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-delegate int NumberChanger(int n);
-delegate int NumberGetter();
+//delegate int NumberChanger(int n);
+//delegate int NumberGetter();
+//namespace DelegateAppl
+//{
+//    class TestDelegate
+//    {
+//        static int num = 10;
+
+//        public static int AddNum(int p)
+//        {
+//            num += p;
+//            return num;
+//        }
+
+//        public static int MultNum(int q)
+//        {
+//            num *= q;
+//            return num;
+//        }
+
+//        public static int GetNum()
+//        {
+//            return num;
+//        }
+
+//        static void Main()
+//        {
+//            // 创建委托实例
+//            NumberChanger nc;
+//            NumberChanger nc1 = new NumberChanger(AddNum);
+//            NumberChanger nc2 = new NumberChanger(MultNum);
+//            NumberGetter ng = new NumberGetter(GetNum);
+//            nc = nc1;
+//            nc += nc2;
+
+//            // 调用多播
+//            nc(5);
+//            Console.WriteLine("Value of Num: {0}", ng());
+
+//            //使用委托对象调用方法
+//            //nc1(25);
+//            //Console.WriteLine("Value of Num: {0}", ng());
+//            //nc2(5);
+//            //Console.WriteLine("Value of Num: {0}", ng());
+//            Console.ReadKey();
+//        }
+//    }
+//}
+
 namespace DelegateAppl
 {
-    class TestDelegate
+    class PrintString
     {
-        static int num = 10;
+        static FileStream fs;
+        static StreamWriter sw;
 
-        public static int AddNum(int p)
+        // 委托声明
+        public delegate void printString(string s);
+
+        // 该方法打印到控制台
+        public static void WriteToScreen(string str)
         {
-            num += p;
-            return num;
+            Console.WriteLine("The String is: {0}", str);
         }
 
-        public static int MultNum(int q)
+        // 该方法打印到文件
+        public static void WriteToFile(string s)
         {
-            num *= q;
-            return num;
+            fs = new FileStream("message.txt", FileMode.Append, FileAccess.Write);
+            sw = new StreamWriter(fs);
+            sw.WriteLine(s);
+            sw.Flush();
+            sw.Close();
+            fs.Close();
         }
 
-        public static int GetNum()
+        // 该方法把委托作为参数，并使用它调用方法
+        public static void SendString(printString ps)
         {
-            return num;
+            ps("Hello World");
         }
-
+        
         static void Main()
         {
-            // 创建委托实例
-            NumberChanger nc;
-            NumberChanger nc1 = new NumberChanger(AddNum);
-            NumberChanger nc2 = new NumberChanger(MultNum);
-            NumberGetter ng = new NumberGetter(GetNum);
-            nc = nc1;
-            nc += nc2;
-
-            // 调用多播
-            nc(5);
-            Console.WriteLine("Value of Num: {0}", ng());
-
-            //使用委托对象调用方法
-            //nc1(25);
-            //Console.WriteLine("Value of Num: {0}", ng());
-            //nc2(5);
-            //Console.WriteLine("Value of Num: {0}", ng());
+            printString ps1 = new printString(WriteToScreen);
+            printString ps2 = new printString(WriteToFile);
+            SendString(ps1);
+            SendString(ps2);
             Console.ReadKey();
         }
+
     }
 }
