e33e809022e91513468a53ffea8ff04c4da32c7d
Gaolbirdy <gaolbird@yeah.net>
2017-12-10 22:30:45 +0800

    commit

--- MSDN 语言介绍/类和对象/Program.cs
+++ MSDN 语言介绍/类和对象/Program.cs
@@ -3,37 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
-namespace 类和对象
+//namespace 类和对象
+//{
+//    public class Point
+//    {
+//        public int x, y;
+
+//        public Point(int x, int y)
+//        {
+//            this.x = x;
+//            this.y = y;
+//        }
+
+//        static void Main()
+//        {
+//            Point p1 = new Point(0, 0);
+//            Point p21 = new Point(10, 0);
+//        }
+//    }
+//}
+
+//namespace 类型参数
+//{
+//    public class Pair<TFirst, TSecond>
+//    {
+//        public TFirst First;
+//        public TSecond Second;
+//    }
+
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            Pair<int, string> pair = new Pair<int, string>()
+//            {
+//                First = 1,
+//                Second = "Two"
+//            };
+//            //Pair<char, bool> pair2 = new Pair<char, bool>();
+//            WriteLine(pair.First);
+//            WriteLine(pair.Second);
+//            int i = pair.First; // TFirst is int
+//            string s = pair.Second; // TSecond is string
+//            WriteLine(i);
+//            WriteLine(s);
+//            ReadKey();
+//        }
+//    }
+//}
+
+namespace 基类
 {
-    //public class Point
-    //{
-    //    public int x, y;
-
-    //    public Point(int x, int y)
-    //    {
-    //        this.x = x;
-    //        this.y = y;
-    //    }
-
-    //    static void Main()
-    //    {
-    //        Point p1 = new Point(0, 0);
-    //        Point p21 = new Point(10, 0);
-    //    }
-    //}
-
-    public class Pair<TFirst, TSecond>
+    public class Point
     {
-        public TFirst First;
-        public TSecond second;
+        public int x, y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public class Point3D : Point
+    {
+        public int z;
+        
+        public Point3D(int x, int y, int z) : base(x, y)
+        {
+            this.z = z;
+        }
 
         static void Main()
         {
-            Pair<int,string> pair = new Pair<int, string> { First = 1, second = "two" };
-            //Pair<char,bool> pair2 = new Pair<char, bool>();
-            int i = pair.First; // TFirst is int
-            string s = pair.second; // TSecond is string
+            Point a = new Point(10, 20);
+            Point b = new Point3D(10, 20, 30);
         }
     }
 }
