d2e94759cc4ed549ff1982e5906c822984d2fbf7
Gaolbirdy <gaolbird@yeah.net>
2017-12-11 06:00:42 +0800

    commit

--- MSDN 语言介绍/类和对象/Program.cs
+++ MSDN 语言介绍/类和对象/Program.cs
@@ -54,32 +54,91 @@ using static System.Console;
 //    }
 //}
 
-namespace 基类
+//namespace 基类
+//{
+//    public class Point
+//    {
+//        public int x, y;
+
+//        public Point(int x, int y)
+//        {
+//            this.x = x;
+//            this.y = y;
+//        }
+//    }
+
+//    public class Point3D : Point
+//    {
+//        public int z;
+
+//        public Point3D(int x, int y, int z) : base(x, y)
+//        {
+//            this.z = z;
+//        }
+
+//        static void Main()
+//        {
+//            Point a = new Point(10, 20);
+//            Point b = new Point3D(10, 20, 30);
+//            ReadKey();
+//        }
+//    }
+//}
+
+//namespace 字段
+//{
+//    public class Color
+//    {
+//        public static readonly Color Black = new Color(0, 0, 0);
+//        public static readonly Color White = new Color(255, 255, 255);
+//        public static readonly Color Red = new Color(255, 0, 0);
+//        public static readonly Color Green = new Color(0, 255, 0);
+//        public static readonly Color Blue = new Color(0, 0, 255);
+
+//        private byte r, g, b;
+
+//        public Color(byte r, byte g, byte b)
+//        {
+//            this.r = r;
+//            this.g = g;
+//            this.b = b;
+//        }
+//    }
+//}
+
+namespace 方法
 {
-    public class Point
+    //class RefExample
+    //{
+    //    static void Swap(ref int x, ref int y)
+    //    {
+    //        int temp = x;
+    //        x = y;
+    //        y = temp;
+    //    }
+
+    //    public static void Main()
+    //    {
+    //        int i = 1, j = 2;
+    //        Swap(ref i, ref j);
+    //        WriteLine($"i: {i} j: {j}");
+    //        ReadKey();
+    //    }
+    //}
+
+    class OutExample
     {
-        public int x, y;
-
-        public Point(int x, int y)
+        static void Divide(int x, int y, out int result, out int remainder)
         {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    public class Point3D : Point
-    {
-        public int z;
-        
-        public Point3D(int x, int y, int z) : base(x, y)
-        {
-            this.z = z;
+            result = x / y;
+            remainder = x % y;
         }
 
-        static void Main()
+        public static void Main()
         {
-            Point a = new Point(10, 20);
-            Point b = new Point3D(10, 20, 30);
+            Divide(10, 3, out int res, out int rem);
+            WriteLine("res: {0} rem: {1}", res, rem);
+            ReadKey();
         }
     }
 }
