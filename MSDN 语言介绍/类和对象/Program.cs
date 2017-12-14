cab4670a351e062015b8dc338421f0141fe3f46f
Gaolbirdy <gaolbird@yeah.net>
2017-12-14 23:33:55 +0800

    commit

--- MSDN 语言介绍/类和对象/Program.cs
+++ MSDN 语言介绍/类和对象/Program.cs
@@ -376,6 +376,67 @@ namespace 其他函数成员
 {
     public class List<T>
     {
+        // Constant
+        const int defaultCapacity = 4;
 
+        // Fields
+        T[] items;
+        int count;
+
+        // Constructor
+        public List(int capacity = defaultCapacity)
+        {
+            items = new T[capacity];
+        }
+
+        // Properties
+        public int Count => count;
+
+        public int Capacity
+        {
+            get { return items.Length; }
+            set
+            {
+                if (value < count) value = count;
+                if (value != items.Length)
+                {
+                    T[] newItems = new T[value];
+                    Array.Copy(items, 0, newItems, 0, count);
+                    items = newItems;
+                }
+            }
+        }
+
+        // Indexer
+        public T this[int index]
+        {
+            get
+            {
+                return items[index];
+            }
+            set
+            {
+                items[index] = value;
+                OnChanged();
+            }
+        }
+
+        // Methods
+        public void Add(T item)
+        {
+            if (count == Capacity) Capacity = count * 2;
+            items[count] = item;
+            count++;
+            OnChanged();
+        }
+
+        protected virtual void OnChanged() =>
+            Changed?.Invoke(this, EventArgs.Empty);
+
+        public override bool Equals(object other) =>
+            Equals(this, other as List<T>);
+
+        // Event
+        public event EventHandler Changed;
     }
 }
