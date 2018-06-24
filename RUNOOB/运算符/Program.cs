9ee695424a7124efa0dfbf75cd9aa372a195e35d
Gaolbirdy <gaolbird@yeah.net>
2018-06-24 19:56:59 +0800

    commit

--- RUNOOB/运算符/Program.cs
+++ RUNOOB/运算符/Program.cs
@@ -223,27 +223,27 @@ namespace OperatorsAppl
         //    Console.ReadLine();
         //}
 
-        static void Main(string[] args)
-        {
-            int a = 20;
-            int b = 10;
-            int c = 15;
-            int d = 5;
-            int e;
+        //static void Main(string[] args)
+        //{
+        //    int a = 20;
+        //    int b = 10;
+        //    int c = 15;
+        //    int d = 5;
+        //    int e;
 
-            e = (a + b) * c / d;
-            Console.WriteLine("(a + b) * c / d 的值是 {0}", e);
+        //    e = (a + b) * c / d;
+        //    Console.WriteLine("(a + b) * c / d 的值是 {0}", e);
 
-            e = ((a + b) * c) / d;
-            Console.WriteLine("((a + b) * c) / d 的值是 {0}", e);
+        //    e = ((a + b) * c) / d;
+        //    Console.WriteLine("((a + b) * c) / d 的值是 {0}", e);
 
-            e = (a + b) * (c / d);
-            Console.WriteLine("(a + b) * (c / d) 的值是 {0}", e);
+        //    e = (a + b) * (c / d);
+        //    Console.WriteLine("(a + b) * (c / d) 的值是 {0}", e);
 
-            e = a + (b * c) / d;
-            Console.WriteLine("a + (b * c) / d 的值是 {0}", e);
+        //    e = a + (b * c) / d;
+        //    Console.WriteLine("a + (b * c) / d 的值是 {0}", e);
 
-            Console.ReadKey();
-        }
+        //    Console.ReadKey();
+        //}
     }
 }
