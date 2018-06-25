1fd1dcc60b7df86d191a4a893e61c314519e731c
liukuo <liukuo@gitlab.com>
2018-06-25 08:10:15 +0800

    commit

---
+++ yiibai/决策结构/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DecisionMaking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 1;
+
+            //if (a < 10)
+            //{
+            //    WriteLine("a is less than 10");
+            //}
+            //WriteLine("value of a is : {0}", a);
+
+            //int a = 199;
+
+            //if (a < 10)
+            //{
+            //    WriteLine("a is less than 10");
+            //}
+            //else
+            //{
+            //    WriteLine("a is not less than 10");
+            //}
+            //WriteLine("value of a is : {0}", a);
+
+            //int a = 199;
+
+            //if (a == 19)
+            //{
+            //    WriteLine("Value of a is 19");
+            //}
+            //else if (a == 29)
+            //{
+            //    WriteLine("Value of a is 29");
+            //}
+            //else if (a == 39)
+            //{
+            //    WriteLine("Value of a is 39");
+            //}
+            //else
+            //{
+            //    WriteLine("None of the values is matching");
+            //}
+            //WriteLine("Exact value of a is :  {0}", a);
+
+            //int a = 199;
+            //int b = 299;
+
+            //if (a == 199)
+            //{
+            //    if (b == 299)
+            //    {
+            //        WriteLine("Value of a is 199 and b is 299");
+            //    }
+            //}
+            //WriteLine("Exact value of a is : {0}", a);
+            //WriteLine("Exact value of b is : {0}", b);
+
+            char grade = 'B';
+
+            switch (grade)
+            {
+                case 'A':
+                    WriteLine("Excellent!");
+                    break;
+                case 'B':
+                case 'C':
+                    WriteLine("Well done");
+                    break;
+                case 'D':
+                    WriteLine("You passed");
+                    break;
+                case 'F':
+                    WriteLine("Better try again");
+                    break;
+                default:
+                    WriteLine("Invalid grade");
+                    break;
+            }
+            WriteLine("Your grade is {0}", grade);
+
+            ReadLine();
+        }
+    }
+}
--- yiibai/运算符/Program.cs
+++ yiibai/运算符/Program.cs
@@ -68,8 +68,124 @@ namespace OperatorsAppl
             //    WriteLine("Line 5 - b is either greater than or equal to a");
             //}
 
-            bool a = true;
-            bool b = true;
+            //bool a = true;
+            //bool b = true;
+
+            //if (a && b)
+            //{
+            //    WriteLine("Line 1 - Condition is true");
+            //}
+
+            //if (a || b)
+            //{
+            //    WriteLine("Line 2 - Condition is true");
+            //}
+
+            //a = false;
+            //b = true;
+
+            //if (a && b)
+            //{
+            //    WriteLine("Line 3 - Condition is true");
+            //}
+            //else
+            //{
+            //    WriteLine("Line 3 - Condition is not true");
+            //}
+
+            //if (!(a&&b))
+            //{
+            //    WriteLine("Line 4 - Condition is true");
+            //}
+
+            //int a = 60;
+            //int b = 13;
+            //int c = 0;
+
+            //c = a & b;
+            //WriteLine("Line 1 - Value of c is {0}", c);
+
+            //c = a | b;
+            //WriteLine("Line 2 - Value of c is {0}", c);
+
+            //c = a ^ b;
+            //WriteLine("Line 3 - Value of c is {0}", c);
+
+            //c = ~a;
+            //WriteLine("Line 4 - Value of c is {0}", c);
+
+            //c = a << 2;
+            //WriteLine("Line 5 - Value of c is {0}", c);
+
+            //c = a >> 2;
+            //WriteLine("Line 6 - Value of c is {0}", c);
+
+            //int a = 21;
+            //int c;
+
+            //c = a;
+            //WriteLine("Line 1 - = Value of c = {0}", c);
+
+            //c += a;
+            //WriteLine("Line 2 - += Value of c = {0}", c);
+
+            //c -= a;
+            //WriteLine("Line 3 - -= Value of c = {0}", c);
+
+            //c *= a;
+            //WriteLine("Line 4 - *= Value of c = {0}", c);
+
+            //c /= a;
+            //WriteLine("Line 5 - /= Value of c = {0}", c);
+
+            //c = 200;
+            //c %= a;
+            //WriteLine("Line 6 - %= Value of c = {0}", c);
+
+            //c <<= 2;
+            //WriteLine("Line 7 - <<= Value of c = {0}", c);
+
+            //c >>= 2;
+            //WriteLine("Line 8 - >>= Value of c = {0}", c);
+
+            //c &= 2;
+            //WriteLine("Line 9 - &= Value of c = {0}", c);
+
+            //c ^= 2;
+            //WriteLine("Line 10 - ^= Value of c = {0}", c);
+
+            //c |= 2;
+            //WriteLine("Line 11 - |= Value of c = {0}", c);
+
+            //WriteLine("The size of int is {0}", sizeof(int));
+            //WriteLine("The size of short is {0}", sizeof(short));
+            //WriteLine("The size of double is {0}", sizeof(double));
+
+            //int a, b;
+            //a = 10;
+            //b = (a == 1) ? 20 : 30;
+            //WriteLine("Value of b is {0}", b);
+
+            //b = (a == 10) ? 20 : 30;
+            //WriteLine("Value of b is {0}", b);
+
+            int a = 20;
+            int b = 10;
+            int c = 15;
+            int d = 5;
+            int e;
+
+            e = (a + b) * c / d;
+            WriteLine("Value of (a + b) * c / d is : {0}", e);
+
+            e = ((a + b) * c) / d;
+            WriteLine("Value of ((a + b) * c) / d is : {0} ", e);
+
+            e = (a + b) * (c / d);
+            WriteLine("Value of (a + b) * (c / d) is : {0}", e);
+
+            e = a + (b * c) / d;
+            WriteLine("Value of a + (b * c) / d is : {0}", e);
 
             ReadLine();
         }
