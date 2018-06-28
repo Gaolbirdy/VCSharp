1420da8dd738aedd9ab63095dcd9767175888f48
liukuo <liukuo@gitlab.com>
2018-06-28 08:11:07 +0800

    commit

--- yiibai/枚举/Program.cs
+++ yiibai/枚举/Program.cs
@@ -3,13 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
-namespace 枚举
+namespace EnumApplication
 {
-    class Program
+    //class EnumProgram
+    //{
+    //    enum Days
+    //    {
+    //        Sun,
+    //        Mon,
+    //        Tue,
+    //        Wed,
+    //        Thu,
+    //        Fri,
+    //        Sat
+    //    }
+
+    //    static void Main(string[] args)
+    //    {
+    //        int WeekdayStart = (int)Days.Mon;
+    //        int WeekdayEnd = (int)Days.Fri;
+    //        WriteLine("Monday: {0}", WeekdayStart);
+    //        WriteLine("Friday: {0}", WeekdayEnd);
+    //    }
+    //}
+
+    //public class EnumExample
+    //{
+    //    public enum Season
+    //    {
+    //        WINTER = 10,
+    //        SPRING,
+    //        SUMMER,
+    //        FALL
+    //    }
+
+    //    public static void Main()
+    //    {
+    //        int x = (int)Season.WINTER;
+    //        int y = (int)Season.SUMMER;
+    //        WriteLine("WINTER = {0}", x);
+    //        WriteLine("SUMMER = {0}", y);
+    //    }
+    //}
+
+    public class EnumExample
     {
-        static void Main(string[] args)
+        public enum Days
         {
+            Sun,
+            Mon,
+            Tue,
+            Wed,
+            Thu,
+            Fri,
+            Sat
+        }
+
+        public static void Main()
+        {
+            foreach (string s in Enum.GetNames(typeof(Days)))
+            {
+                WriteLine(s);
+            }
+
+            foreach (Days d in Enum.GetValues(typeof(Days)))
+            {
+                WriteLine(d);
+            }
+
+            foreach (int x in Enum.GetValues(typeof(Days)))
+            {
+                WriteLine(x);
+            }
         }
     }
 }
---
+++ yiibai/类/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace BoxApplication
+{
+    //class Box
+    //{
+    //    public double length;
+    //    public double breadth;
+    //    public double height;
+    //}
+
+    //class BoxTester
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Box box1 = new Box();
+    //        Box box2 = new Box();
+    //        double volume = 0.0;
+
+    //        box1.height = 5.0;
+    //        box1.length = 6.0;
+    //        box1.breadth = 7.0;
+
+    //        box2.height = 10.0;
+    //        box2.length = 12.0;
+    //        box2.breadth = 13.0;
+
+    //        volume = box1.height * box1.length * box1.breadth;
+    //        WriteLine("Volume of Box1 : {0}", volume);
+
+    //        volume = box2.height * box2.length * box2.breadth;
+    //        WriteLine("Volume of Box2 : {0}", volume);
+
+    //    }
+    //}
+
+    //class Box
+    //{
+    //    private double length;
+    //    private double breadth;
+    //    private double height;
+
+    //    public void SetLength(double len)
+    //    {
+    //        length = len;
+    //    }
+
+    //    public void SetBreadth(double bre)
+    //    {
+    //        breadth = bre;
+    //    }
+
+    //    public void SetHeight(double hei)
+    //    {
+    //        height = hei;
+    //    }
+
+    //    public double GetVolume()
+    //    {
+    //        return length * breadth * height;
+    //    }
+    //}
+
+    //class BoxTester
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Box box1 = new Box();
+    //        Box box2 = new Box();
+    //        double volume;
+
+    //        box1.SetLength(6.0);
+    //        box1.SetBreadth(7.0);
+    //        box1.SetHeight(5.0);
+
+    //        box2.SetLength(12.0);
+    //        box2.SetBreadth(13.0);
+    //        box2.SetHeight(10.0);
+
+    //        volume = box1.GetVolume();
+    //        WriteLine("Volume of Box1 : {0}", volume);
+
+    //        volume = box2.GetVolume();
+    //        WriteLine("Volume of Box2 : {0}", volume);
+
+    //    }
+    //}
+}
+
+//namespace LineApplication
+//{
+//    class Line
+//    {
+//        private double length;
+
+//        public Line()
+//        {
+//            WriteLine("Object is being created");
+//        }
+
+//        public Line(double len)
+//        {
+//            WriteLine("Object is being created, length = {0}", len);
+//            length = len;
+//        }
+
+//        ~Line()
+//        {
+//            WriteLine("Object is being deleted");
+//        }
+
+//        public void SetLength(double len)
+//        {
+//            length = len;
+//        }
+
+//        public double GetLength()
+//        {
+//            return length;
+//        }
+
+//        static void Main(string[] args)
+//        {
+//            Line line = new Line(100.0);
+//            WriteLine("Length of line : {0}", line.GetLength());
+
+//            line.SetLength(60.0);
+//            WriteLine("Length of line : {0}", line.GetLength());
+//        }
+//    }
+//}
+
+namespace StaticVarApplication
+{
+    class StaticVar
+    {
+        public static int num;
+
+        public void Count()
+        {
+            num++;
+        }
+
+        public int GetNum()
+        {
+            return num;
+        }
+    }
+
+    class StaticTester
+    {
+        static void Main(string[] args)
+        {
+            StaticVar s1 = new StaticVar();
+            StaticVar s2 = new StaticVar();
+
+            s1.Count();
+            s1.Count();
+            s1.Count();
+            s2.Count();
+            WriteLine("Variable num for s1: {0}", s1.GetNum());
+            WriteLine("Variable num for s2: {0}", s2.GetNum());
+        }
+    }
+}
