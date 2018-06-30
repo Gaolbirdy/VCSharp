62e52bd7618c31ba054e0915feaadb7d47c19bb6
Gaolbirdy <gaolbird@yeah.net>
2018-06-30 22:25:18 +0800

    commit

--- yiibai/特性/Program.cs
+++ yiibai/特性/Program.cs
@@ -1,15 +1,42 @@
-﻿using System;
+﻿#define DEBUG
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
+using System.Diagnostics;
 
 namespace 特性
 {
-    class Program
+    public class MyClass
     {
-        static void Main(string[] args)
+        [Conditional("DEBUG")]
+        public static void Message(string msg)
         {
+            WriteLine(msg);
+        }
+    }
+
+    class Test
+    {
+        static void Function1()
+        {
+            MyClass.Message("In Function 1.");
+            Function2();
+        }
+
+        static void Function2()
+        {
+            MyClass.Message("In Function 2.");
+        }
+
+        public static void Main()
+        {
+            MyClass.Message("In Main Function.");
+            Function1();
+            ReadKey();
         }
     }
 }
