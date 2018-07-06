63db465d1519b7940a3f62224cf602a0b4414209
liukuo <liukuo@gitlab.com>
2018-07-06 08:33:13 +0800

    commit

--- RUNOOB/事件/Program.cs
+++ RUNOOB/事件/Program.cs
@@ -4,177 +4,255 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using static System.Console;
 
-//namespace SimpleEvent
+////namespace SimpleEvent
+////{
+////    /***********发布器类***********/
+////    public class EventTest
+////    {
+////        private int value;
+
+////        public delegate void NumManipulationHandler();
+
+////        public event NumManipulationHandler ChangeNum;
+
+////        protected virtual void OnNumChanged()
+////        {
+////            if (ChangeNum != null)
+////            {
+////                ChangeNum();    /* 事件被触发 */
+////            }
+////            else
+////            {
+////                Console.WriteLine("event not fire");
+////                Console.ReadKey();
+////            }
+////        }
+
+////        //public bool HasEvent()
+////        //{
+////        //    return (ChangeNum != null) ? true : false;
+////        //}
+
+////        public EventTest()
+////        {
+////            int n = 5;
+////            SetValue(n);
+////        }
+
+////        public void SetValue(int n)
+////        {
+////            if (value != n)
+////            {
+////                value = n;
+////                OnNumChanged();
+////            }
+////        }
+////    }
+
+////    /***********订阅器类***********/
+////    public class SubscribEvent
+////    {
+////        public void PrintF()
+////        {
+////            Console.WriteLine("event fire");
+////            Console.ReadKey();
+////        }
+////    }
+
+////    /***********触发***********/
+////    public class MainClass
+////    {
+////        public static void Main()
+////        {
+////            EventTest e = new EventTest();  /* 实例化对象,第一次没有触发事件 */
+////            SubscribEvent v = new SubscribEvent();  /* 实例化对象 */
+////            e.ChangeNum += new EventTest.NumManipulationHandler(v.PrintF);  /* 注册 */
+////            e.SetValue(7);
+////            e.SetValue(11);
+////            e.SetValue(11);
+////        }
+////    }
+////}
+
+//namespace BoilerEventAppl
+//// 本实例提供一个简单的用于热水锅炉系统故障排除的应用程序。当维修工程师检查锅炉时，锅炉的温度和压力会随着维修工程师的备注自动记录到日志文件中。
 //{
-//    /***********发布器类***********/
-//    public class EventTest
+//    // boiler 类
+//    class Boiler
 //    {
-//        private int value;
+//        private int temp;
+//        private int pressure;
 
-//        public delegate void NumManipulationHandler();
-
-//        public event NumManipulationHandler ChangeNum;
-
-//        protected virtual void OnNumChanged()
+//        public Boiler(int t, int p)
 //        {
-//            if (ChangeNum != null)
-//            {
-//                ChangeNum();    /* 事件被触发 */
-//            }
-//            else
-//            {
-//                Console.WriteLine("event not fire");
-//                Console.ReadKey();
-//            }
+//            temp = t;
+//            pressure = p;
 //        }
 
-//        //public bool HasEvent()
-//        //{
-//        //    return (ChangeNum != null) ? true : false;
-//        //}
-
-//        public EventTest()
+//        public int GetTemp()
 //        {
-//            int n = 5;
-//            SetValue(n);
+//            return temp;
 //        }
 
-//        public void SetValue(int n)
+//        public int GetPressure()
 //        {
-//            if (value != n)
+//            return pressure;
+//        }
+//    }
+
+//    // 事件发布器
+//    class DelegateBoilerEvent
+//    {
+//        public delegate void BoilerLogHandler(string status);
+
+//        // 基于上面的委托定义事件
+//        public event BoilerLogHandler BoilerEventLog;
+
+//        public void LogProcess()
+//        {
+//            string remarks = "O. K";
+//            Boiler b = new Boiler(100, 12);
+//            int t = b.GetTemp();
+//            int p = b.GetPressure();
+//            if (t > 150 || t < 80 || p < 12 || p > 15)
 //            {
-//                value = n;
-//                OnNumChanged();
+//                remarks = "Need Maintenance";
+//            }
+//            OnBoilerEventLog("Logging Info:\n");
+//            OnBoilerEventLog("Temparature " + t + "\nPressure: " + p);
+//            OnBoilerEventLog("\nMessage: " + remarks);
+//        }
+
+//        protected void OnBoilerEventLog(string message)
+//        {
+//            if (BoilerEventLog != null)
+//            {
+//                BoilerEventLog(message);
 //            }
 //        }
 //    }
 
-//    /***********订阅器类***********/
-//    public class SubscribEvent
+//    // 该类保留写入日志文件的条款
+//    class BoilerInfoLogger
 //    {
-//        public void PrintF()
+//        FileStream fs;
+//        StreamWriter sw;
+
+//        public BoilerInfoLogger(string filename)
 //        {
-//            Console.WriteLine("event fire");
-//            Console.ReadKey();
+//            fs = new FileStream(filename, FileMode.Append, FileAccess.Write);
+//            sw = new StreamWriter(fs);
+//        }
+
+//        public void Logger(string info)
+//        {
+//            sw.WriteLine(info);
+//        }
+
+//        public void Close()
+//        {
+//            sw.Close();
+//            fs.Close();
 //        }
 //    }
 
-//    /***********触发***********/
-//    public class MainClass
+//    // 事件订阅器
+//    public class RecordBoilerInfo
 //    {
-//        public static void Main()
+//        static void Logger(string info)
 //        {
-//            EventTest e = new EventTest();  /* 实例化对象,第一次没有触发事件 */
-//            SubscribEvent v = new SubscribEvent();  /* 实例化对象 */
-//            e.ChangeNum += new EventTest.NumManipulationHandler(v.PrintF);  /* 注册 */
-//            e.SetValue(7);
-//            e.SetValue(11);
-//            e.SetValue(11);
-//        }
-//    }
+//            Console.WriteLine(info);
+//        }//end of Logger
+
+//        static void Main()
+//        {
+//            BoilerInfoLogger fileLog = new BoilerInfoLogger("boiler.txt");
+//            DelegateBoilerEvent boilerEvent = new DelegateBoilerEvent();
+//            boilerEvent.BoilerEventLog += new DelegateBoilerEvent.BoilerLogHandler(Logger);
+//            boilerEvent.BoilerEventLog += new DelegateBoilerEvent.BoilerLogHandler(fileLog.Logger);
+//            boilerEvent.LogProcess();
+//            Console.ReadLine();
+//            fileLog.Close();
+//        }//end of main
+
+//    }//end of RecordBoilerInfo
 //}
 
-namespace BoilerEventAppl
-// 本实例提供一个简单的用于热水锅炉系统故障排除的应用程序。当维修工程师检查锅炉时，锅炉的温度和压力会随着维修工程师的备注自动记录到日志文件中。
+namespace SimpleEvent
 {
-    // boiler 类
-    class Boiler
+    public class EventTest
     {
-        private int temp;
-        private int pressure;
+        private int value;
 
-        public Boiler(int t, int p)
+        public delegate void NumManipulationHandler();
+
+        public event NumManipulationHandler ChangeNum;
+
+        protected virtual void OnNumChanged()
         {
-            temp = t;
-            pressure = p;
-        }
-
-        public int GetTemp()
-        {
-            return temp;
-        }
-
-        public int GetPressure()
-        {
-            return pressure;
-        }
-    }
-
-    // 事件发布器
-    class DelegateBoilerEvent
-    {
-        public delegate void BoilerLogHandler(string status);
-
-        // 基于上面的委托定义事件
-        public event BoilerLogHandler BoilerEventLog;
-
-        public void LogProcess()
-        {
-            string remarks = "O. K";
-            Boiler b = new Boiler(100, 12);
-            int t = b.GetTemp();
-            int p = b.GetPressure();
-            if (t > 150 || t < 80 || p < 12 || p > 15)
+            if (ChangeNum != null)
             {
-                remarks = "Need Maintenance";
+                ChangeNum();
             }
-            OnBoilerEventLog("Logging Info:\n");
-            OnBoilerEventLog("Temparature " + t + "\nPressure: " + p);
-            OnBoilerEventLog("\nMessage: " + remarks);
-        }
-
-        protected void OnBoilerEventLog(string message)
-        {
-            if (BoilerEventLog != null)
+            else
             {
-                BoilerEventLog(message);
+                WriteLine("null -> event not fire");
+                ReadKey();
             }
         }
-    }
 
-    // 该类保留写入日志文件的条款
-    class BoilerInfoLogger
-    {
-        FileStream fs;
-        StreamWriter sw;
-
-        public BoilerInfoLogger(string filename)
+        public EventTest()
         {
-            fs = new FileStream(filename, FileMode.Append, FileAccess.Write);
-            sw = new StreamWriter(fs);
+            int n = 5;
+            SetValue(n);
         }
 
-        public void Logger(string info)
+        public void SetValue(int n)
         {
-            sw.WriteLine(info);
+            if (value != n)
+            {
+                value = n;
+                OnNumChanged();
+            }
+            else
+                WriteLine(value + " equal -> event not fire");
         }
 
-        public void Close()
+        public void PrintValue()
         {
-            sw.Close();
-            fs.Close();
+            WriteLine(value);
         }
     }
 
-    // 事件订阅器
-    public class RecordBoilerInfo
+    public class SubscribeEvent
     {
-        static void Logger(string info)
+        public void Printf()
         {
-            Console.WriteLine(info);
-        }//end of Logger
+            WriteLine("event fire");
+            ReadKey();
+        }
+    }
 
-        static void Main()
+    public class MainClass
+    {
+        public static void Main()
         {
-            BoilerInfoLogger fileLog = new BoilerInfoLogger("boiler.txt");
-            DelegateBoilerEvent boilerEvent = new DelegateBoilerEvent();
-            boilerEvent.BoilerEventLog += new DelegateBoilerEvent.BoilerLogHandler(Logger);
-            boilerEvent.BoilerEventLog += new DelegateBoilerEvent.BoilerLogHandler(fileLog.Logger);
-            boilerEvent.LogProcess();
-            Console.ReadLine();
-            fileLog.Close();
-        }//end of main
+            EventTest e = new EventTest();
 
-    }//end of RecordBoilerInfo
+            SubscribeEvent v = new SubscribeEvent();
+
+            e.ChangeNum += new EventTest.NumManipulationHandler(v.Printf);
+            //e.ChangeNum += v.Printf;
+            e.SetValue(5);
+            e.SetValue(7);
+            e.SetValue(11);
+            e.SetValue(11);
+            e.ChangeNum -= v.Printf;
+            e.SetValue(13);
+
+        }
+    }
 }
+
---
+++ yiibai/事件/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SampleApp
+{
+    public delegate string MyDel(string str);
+
+    class EventProgram
+    {
+        event MyDel MyEvent;
+        //MyDel MyEvent;
+
+        public EventProgram()
+        {
+            this.MyEvent += new MyDel(this.WelcomeUser);
+        }
+
+        public string WelcomeUser(string username)
+        {
+            return "Welcome " + username;
+        }
+
+        static void Main(string[] args)
+        {
+            EventProgram obj1 = new EventProgram();
+            string result = obj1.MyEvent("Yiibai Yiibai");
+            WriteLine(result);
+        }
+    }
+}
