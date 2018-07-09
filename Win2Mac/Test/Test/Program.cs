e60a25a7eb45d7dbb170298fb8838825562b43a4
liukuo <liukuo@gitlab.com>
2018-07-10 07:13:42 +0800

    commit

---
+++ Win2Mac/Test/Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Test Win to Mac.");
+            ReadKey();
+        }
+    }
+}
